ea1a01d553d5a7601788456e44a58dc048116e22
00011655wiutstudent <00011655wiutstudent@gmail.com>
2023-04-03 09:44:48 +0500

    3

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 
 app.set ('view engine', 'pug')
+app.use ('/static', express.static('public'))
 app.get('/', (req, res) => {
-    res.send("Hello")
+    res.render('index')
 })
-
+app.get('/create', (req, res) =>{
+    app.render('create')
+})
 app.listen(8000, err => {
     if (err) console.log(err)
 
